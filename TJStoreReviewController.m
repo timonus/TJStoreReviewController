43d67155163e76702246fb9779f6ab0cb243be71
Tim Johnsen <timonus@gmail.com>
2018-09-09 12:30:06 -0700

    Skip compiling outdated methods instead of suppressing warnings.

--- TJStoreReviewController.m
+++ TJStoreReviewController.m
@@ -75,14 +75,15 @@ static const NSUInteger kTJStoreReviewControllerSubsequentDaysToRate = 30;
 + (void)openURLString:(NSString *const)urlString NS_EXTENSION_UNAVAILABLE_IOS("+reviewInAppStore: isn't available in app extensions because it requires a UIApplication instance and -openURL:")
 {
     NSURL *const url = [NSURL URLWithString:urlString];
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0
     if (@available(iOS 10.0, *)) {
+#endif
         [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0
     } else {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         [[UIApplication sharedApplication] openURL:url];
-#pragma clang diagnostic pop
     }
+#endif
 }
 
 @end
