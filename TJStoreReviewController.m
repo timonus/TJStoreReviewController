9d29ab09b908518a6f56eecfe09b598d19c456ad
Tim Johnsen <timonus@gmail.com>
2019-12-17 21:07:56 -0800

    Use C functions in place of Obj-C methods.

--- TJStoreReviewController.m
+++ TJStoreReviewController.m
@@ -19,7 +19,7 @@ static const NSUInteger kTJStoreReviewControllerSubsequentDaysToRate = 30;
 {
     NSDate *const date = [[NSUserDefaults standardUserDefaults] objectForKey:kTJStoreReviewControllerNextReviewDateKey];
     if (!date) {
-        [self deferNextRateDayByDaysFromPresent:kTJStoreReviewControllerInitialDaysToRate];
+        deferNextRateDayByDaysFromPresent(kTJStoreReviewControllerInitialDaysToRate);
     }
 }
 
@@ -28,10 +28,10 @@ static const NSUInteger kTJStoreReviewControllerSubsequentDaysToRate = 30;
     BOOL didTryShow = NO;
     NSDate *const date = [[NSUserDefaults standardUserDefaults] objectForKey:kTJStoreReviewControllerNextReviewDateKey];
     if (!date) {
-        [self deferNextRateDayByDaysFromPresent:kTJStoreReviewControllerInitialDaysToRate];
+        deferNextRateDayByDaysFromPresent(kTJStoreReviewControllerInitialDaysToRate);
     } else {
         if ([date earlierDate:[NSDate date]] == date) {
-            [self deferNextRateDayByDaysFromPresent:kTJStoreReviewControllerSubsequentDaysToRate];
+            deferNextRateDayByDaysFromPresent(kTJStoreReviewControllerSubsequentDaysToRate);
             if (@available(iOS 10.3, *)) {
                 [SKStoreReviewController requestReview];
                 didTryShow = YES;
@@ -43,7 +43,7 @@ static const NSUInteger kTJStoreReviewControllerSubsequentDaysToRate = 30;
 
 + (void)reviewInAppStore:(NSString *const)appIdentifierString
 {
-    [self deferNextRateDayByDaysFromPresent:kTJStoreReviewControllerSubsequentDaysToRate];
+    deferNextRateDayByDaysFromPresent(kTJStoreReviewControllerSubsequentDaysToRate);
     NSString *urlFormatString = nil;
     if (@available(iOS 10.3, *)) {
         urlFormatString = @"itms-apps://itunes.apple.com/app/id%@?action=write-review";
@@ -52,16 +52,16 @@ static const NSUInteger kTJStoreReviewControllerSubsequentDaysToRate = 30;
     }
     NSString *urlString = [NSString stringWithFormat:urlFormatString, appIdentifierString];
     
-    [self openURLString:urlString];
+    openURLString(urlString);
 }
 
 + (void)showInAppStore:(NSString *const)appIdentifierString
 {
     NSString *urlString = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/id%@", appIdentifierString];
-    [self openURLString:urlString];
+    openURLString(urlString);
 }
 
-+ (void)deferNextRateDayByDaysFromPresent:(const NSUInteger)daysFromPresent
+static void deferNextRateDayByDaysFromPresent(const NSUInteger daysFromPresent)
 {
     NSDate *deferDate = [NSDate dateWithTimeIntervalSinceNow:3600.0 * 24.0 * daysFromPresent];
     // Floor the input date to the very beginning of the specified day.
@@ -72,7 +72,7 @@ static const NSUInteger kTJStoreReviewControllerSubsequentDaysToRate = 30;
     [[NSUserDefaults standardUserDefaults] setObject:deferDate forKey:kTJStoreReviewControllerNextReviewDateKey];
 }
 
-+ (void)openURLString:(NSString *const)urlString NS_EXTENSION_UNAVAILABLE_IOS("+reviewInAppStore: isn't available in app extensions because it requires a UIApplication instance and -openURL:")
+static void openURLString(NSString *const urlString) NS_EXTENSION_UNAVAILABLE_IOS("+reviewInAppStore: isn't available in app extensions because it requires a UIApplication instance and -openURL:")
 {
     NSURL *const url = [NSURL URLWithString:urlString];
 #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0
